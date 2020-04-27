3d0a1273393a9f4fc48843ca3b9c5a340e4d9304
tim <timdedenker@gmail.com>
2020-04-27 22:14:25 +0200

    major overhaul of logic, implemented various graphical functions, merged file logic

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,12 +1,52 @@
 from tkinter import *
+import threading
+import training_timer as t
 
-
-default_button = 10
+default_button = 8
 button_size_train = -6
 defaultfont = 'TkDefaultFont 10'
 bigfont = 'TkDefaultFont 11 bold'
 
+shots = ["pull", "push", "middle", "near split", "far split", "near cross", "far cross"]
+
 training_selection = 3
+
+pauseswitch = 0
+startswitch = 0
+
+def	get_series_shots(series):
+	if series == "snake":
+		return ["pull", "push", "middle", "near split", "far split", "near cross", "far cross"]
+	if series == "pull":
+		return ["long", "middle", "short", "two", "four", "brush", "john wayne"]
+	if series == "stick" or series == "brush":
+		return ["lane", "wall", "second man", "bounce lane", "bounce wall", "left hook"]
+
+def	handle_pauseplay(_=None):
+	if startswitch:
+		global pauseswitch
+		if pauseswitch == 0:
+			pauseplayB.config(relief=SUNKEN, text="⏯Play")
+		if pauseswitch == 1:
+			pauseplayB.config(relief=RAISED, text="⏯Pause")
+		pauseswitch = 1 - pauseswitch
+		print(pauseswitch)
+
+def	handle_startstop(_=None):
+	global startswitch
+	if startswitch == 0:
+		startswitch = 1 - startswitch
+		startstopB.config(relief=SUNKEN, text="◼ Stop Training")
+		probabilities = [prob.get() for prob in probs[:shot_amount.get()]]
+		shots = get_series_shots(series)
+		training_loopT = threading.Thread(target=t.training_loop, args=(training_selection, t.engine, shotcall_delay, probabilities, shots[:shot_amount.get()], beepdelay.get(), series))
+		training_loopT.daemon = True
+		training_loopT.start()
+	elif startswitch == 1:
+		startstopB.config(relief=RAISED, text="▶ Start Training")
+		if pauseswitch:
+			handle_pauseplay()
+		startswitch = 1 - startswitch
 
 def	press_button(button):
 	twobarB.config(relief=RAISED)
@@ -20,66 +60,158 @@ def	press_button(button):
 	if button == 3:
 		threebarframe.tkraise()
 		threebarB.config(relief=SUNKEN)
-	if button ==5:
+		shot_type_3b.set("snake")
+		shot_amount.set(3)
+		series_button("snake")
+	if button == 5:
 		fivebarframe.tkraise()
 		fivebarB.config(relief=SUNKEN)
+		shot_type_5b.set("brush")
+		shot_amount.set(3)
+		series_button("brush")
+
+def	handle_shotcount():
+	if shot_amount.get() == 3:
+		probslider1.place(x=260, y=50)
+		probslider2.place(x=260, y=100)
+		probslider3.place(x=260, y=150)
+		probslider4.place_forget()
+		probslider5.place_forget()
+		probslider6.place_forget()
+		probslider7.place_forget()
+	if shot_amount.get() == 5:
+		probslider1.place(x=260, y=50)
+		probslider2.place(x=260, y=100)
+		probslider3.place(x=260, y=150)
+		probslider4.place(x=260, y=200)
+		probslider5.place(x=260, y=250)
+		probslider6.place_forget()
+		probslider7.place_forget()
+	if shot_amount.get() == 7:
+		probslider1.place(x=260, y=50)
+		probslider2.place(x=260, y=100)
+		probslider3.place(x=260, y=150)
+		probslider4.place(x=260, y=200)
+		probslider5.place(x=260, y=250)
+		probslider6.place(x=260, y=300)
+		probslider7.place(x=260, y=350)
+
+def	series_button(shot_type):
+	global series
+	series = shot_type
+	if shot_type == "snake" or shot_type == "pull":
+		handle_shotcount()
+	if shot_type == "stick":
+		stick_radio_label.place(x=80, y=30)
+		stick_radio_1.place(x=210, y=30)
+		stick_radio_2.place(x=270, y=30)
+		stick_radio_3.place(x=330, y=30)
+	if shot_type == "brush":
+		shot_amount.set(2)
+		stick_radio_label.place_forget()
+		stick_radio_1.place_forget()
+		stick_radio_2.place_forget()
+		stick_radio_3.place_forget()
 
 mainwindow = Tk()
 
 mainwindow.title("Foos Timer")
-# mainwindow.geometry('700x350')
-# mainwindow.rowconfigure(0, weight=1)
-# mainwindow.columnconfigure(0, weight=1)
+# mainwindow.geometry('450x650')
 
-topframe = Frame(mainwindow)
+probs = [IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar(), IntVar()]
+for prob in probs:
+	prob.set(5)
+
+
+topframe = Frame(mainwindow, width=450, height=100)
 topframe.grid(row=0, column=0)
 
 timerframe = Frame(mainwindow)
-timerframe.grid(row=1, column=0)
+timerframe.grid(row=6, column=0)
 
-twobarframe = Frame(mainwindow)
+twobarframe = Frame(mainwindow, width=450, height=400)
 twobarframe.grid(row=2, column=0)
-threebarframe = Frame(mainwindow)
+threebarframe = Frame(mainwindow, width=450, height=400)
 threebarframe.grid(row=2, column=0)
-fivebarframe = Frame(mainwindow)
+fivebarframe = Frame(mainwindow, width=450, height=400)
 fivebarframe.grid(row=2, column=0)
+
+menuframe = Frame(mainwindow, width=450, height=50)
+menuframe.grid(row=7, column=0)
+
+mainwindow.bind("<KeyPress-space>", handle_pauseplay)
+mainwindow.bind("<KeyPress-s>", handle_startstop)
 
 # TOPFRAME
 twobarB = Button(topframe, text="2-bar", fg="red", width=button_size_train, font=bigfont, command=lambda:press_button(2))
-twobarB.grid(row=0, column=0, pady=15, padx=30, sticky='nsew')
+twobarB.grid(row=0, column=2, pady=15, padx=30, sticky='nsew')
 threebarB = Button(topframe, text="3-bar", fg="blue", width=button_size_train, font=bigfont, command=lambda:press_button(3))
-threebarB.grid(row=0, column=1, pady=15, padx=30, sticky='nsew')
+threebarB.grid(row=0, column=0, pady=15, padx=30, sticky='nsew')
 threebarB.config(relief=SUNKEN)
 fivebarB = Button(topframe, text="5-bar", fg="green", width=button_size_train, font=bigfont, command=lambda:press_button(5))
-fivebarB.grid(row=0, column=2, pady=15, padx=30, sticky='nsew')
+fivebarB.grid(row=0, column=1, pady=15, padx=30, sticky='nsew')
 
 # TIMERFRAME
 beepdelay = DoubleVar()
 beepdelay.set(1.0)
 
-Label(timerframe, text="Beep delay (seconds)", anchor="e", font=defaultfont, width=18).grid(row=0, column=0, sticky='se', padx=30)
+Label(timerframe, text="Beep delay (seconds)", anchor="e", font=defaultfont, width=18).grid(row=1, column=0, sticky='se', padx=30)
 beepdelayW = Scale(timerframe, from_=0, to=3.0, length=200, orient='horizontal', resolution=0.1, variable=beepdelay)
-beepdelayW.grid(row=0, column=1, sticky='se')
+beepdelayW.grid(row=1, column=1, sticky='se')
 
 shotcall_delay = DoubleVar()
 shotcall_delay.set(12.0)
 
-Label(timerframe, text="Shotcall delay (seconds)", anchor="e", font=defaultfont, width=18).grid(row=1, column=0, sticky='se', padx=30)
+Label(timerframe, text="Shotcall delay (seconds)", anchor="e", font=defaultfont, width=18).grid(row=0, column=0, sticky='se', padx=30)
 shotcall_delayW = Scale(timerframe, from_=0, to=20.0, length=200, orient='horizontal', resolution=0.1, variable=shotcall_delay)
-shotcall_delayW.grid(row=1, column=1, sticky='se')
+shotcall_delayW.grid(row=0, column=1, sticky='se')
+
+# MENUFRAME
+startstopB = Button(menuframe, text="▶ Start Training", anchor="e", width=11, font=defaultfont, command=handle_startstop)
+startstopB.grid(row=0, column=0, pady=8, padx=30, sticky='sw')
+pauseplayB = Button(menuframe, text="⏯Pause", anchor="w", width=7, font=defaultfont, command=handle_pauseplay)
+pauseplayB.grid(row=0, column=1, pady=8, padx=50, sticky='se')
 
 # 3-BAR FRAME
-shot_amount = IntVar()
-shot_amount.set(5)
+shot_type_3b = StringVar()
 
-Label(threebarframe, text="Amount of shots", anchor="e", font=defaultfont, width=23).grid(row=0, column=0, sticky='se', padx=10, pady=20)
-Radiobutton(threebarframe, text='3', variable=shot_amount, value=3).grid(row=0, column=1, padx=24, pady=20,sticky='se')
-Radiobutton(threebarframe, text='5', variable=shot_amount, value=5).grid(row=0, column=2, padx=24, pady=20,sticky='se')
-Radiobutton(threebarframe, text='7', variable=shot_amount, value=7).grid(row=0, column=3,padx=24, pady=20, sticky='se')
+Radiobutton(threebarframe, text='Snake Shot', variable=shot_type_3b, value="snake", command=lambda:series_button("snake")).place(x=120, y=0)
+Radiobutton(threebarframe, text='Pull Shot', variable=shot_type_3b, value="pull", command=lambda:series_button("pull")).place(x=230, y=0)
+
+shot_amount = IntVar()
+
+Label(threebarframe, text="Amount of shots", anchor="e", font=defaultfont, width=0).place(x=80, y=30)
+Radiobutton(threebarframe, text='3', variable=shot_amount, value=3, command=handle_shotcount).place(x=210, y=30)
+Radiobutton(threebarframe, text='5', variable=shot_amount, value=5, command=handle_shotcount).place(x=270, y=30)
+Radiobutton(threebarframe, text='7', variable=shot_amount, value=7, command=handle_shotcount).place(x=330, y=30)
+
+probslider1 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[0])
+probslider2 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[1])
+probslider3 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[2])
+probslider4 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[3])
+probslider5 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[4])
+probslider6 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[5])
+probslider7 = Scale(threebarframe, from_=0, to=10, length=120, orient='horizontal', resolution=1, variable=probs[6])
+
+# 5-BAR FRAME
+shot_type_5b = StringVar()
+shot_type_5b.set("brush")
+
+Radiobutton(fivebarframe, text='Brush Series', variable=shot_type_5b, value="brush", command=lambda:series_button("brush")).place(x=120, y=0)
+Radiobutton(fivebarframe, text='Stick Series', variable=shot_type_5b, value="stick", command=lambda:series_button("stick")).place(x=230, y=0)
+
+stick_radio_label = Label(fivebarframe, text="Amount of shots", anchor="e", font=defaultfont, width=0)
+stick_radio_label.place(x=80, y=30)
+stick_radio_1 = Radiobutton(fivebarframe, text='3', variable=shot_amount, value=3)
+stick_radio_1.place(x=210, y=30)
+stick_radio_2 = Radiobutton(fivebarframe, text='5', variable=shot_amount, value=5)
+stick_radio_2.place(x=270, y=30)
+stick_radio_3 = Radiobutton(fivebarframe, text='6', variable=shot_amount, value=6)
+stick_radio_3.place(x=330, y=30)
 
 # 2-BAR FRAME
 
 
-
-
-mainwindow.mainloop()
+if __name__ == "__main__":
+	press_button(3)
+	mainwindow.mainloop()
--- training_timer.py
+++ training_timer.py
@@ -6,6 +6,7 @@ import keyboard
 import threading
 import winsound
 import time
+import __main__ as gui
 
 def	print_banner():
 	print("\
@@ -30,26 +31,25 @@ def loopfor(funct, secs, args=None):
 	while mytimer.get() < secs:
 		funct(args)
 
-def pause(_):
-	global set_pause
-	set_pause = 1
-
 def showusage():
 	print("usage: python training_timer.py\n[space]: pause, [esc]: quit")
 	exit()
 
+class KillTraining(Exception): pass
+
 def check_pause(flag=None):
-	global set_pause
 	global mytimer
 	if flag == None:
 		mytimer.start()
-	if set_pause == 1:
+	if gui.pauseswitch == 1:
 		print("training paused. press space to resume")
 		mytimer.pause()
-		keyboard.wait('space')
+		while gui.pauseswitch == 1:
+			pass
 		mytimer.resume()
 		print("resuming training...")
-		set_pause = 0
+	if gui.startswitch == 0:
+		raise KillTraining
 
 def	output_sentence(sentence, engine, speed):
 	global rate
@@ -57,37 +57,38 @@ def	output_sentence(sentence, engine, speed):
 	engine.say(sentence)
 	engine.runAndWait()
 
-def delayed_beep(offset):
-	global beepdelay
-	time.sleep(beepdelay + 0.05 * (offset - 4))
+def delayed_beep(offset, beepdelay):
+	time.sleep(beepdelay + 0.05 + 0.05 * (offset - 4 if offset > 4 else 0))
 	winsound.Beep(2500, 500)
 
-def training_loop(training, engine, interval_range, weights, calls, shot_series=None):
-	if training == "1":
-		engine.say("start three baar training")
-		print("start 3-bar training")
-	elif training == "2":
-		engine.say("start five baar training")
-		print("start 5-bar training")
-	elif training == "3":
-		engine.say("start two baar training. " + shot_series)
-		print("start 2-bar training: " + shot_series)
-	engine.runAndWait()
-	loopfor(check_pause, 3, 1)
-	global set_pause
-	while 1:
-		check_pause()
-		clock = random.randrange(3, 4 + interval_range)
-		shotcall = random.choices(calls, weights)[0]
-		output_sentence("set up the ball", engine, -10)
-		check_pause()
-		output_sentence("time in", engine, 0)
-		loopfor(check_pause, clock, 1)
-		beep = threading.Thread(target=delayed_beep, args=([len(shotcall)]))
-		beep.start()
-		output_sentence(shotcall, engine, 30)
-		print("%s %*d seconds" %(shotcall, 13 - len(shotcall), clock))
-		loopfor(check_pause, 2.5, 1)
+def training_loop(training, engine, interval_range, weights, calls, beepdelay, shot_series=None):
+	try:
+		if training == 3:
+			engine.say("start three baar training. " + shot_series)
+			print("start 3-bar training: " + shot_series)
+		elif training == 5:
+			engine.say("start five baar training. " + shot_series)
+			print("start 5-bar training: " + shot_series)
+		elif training == 2:
+			engine.say("start two baar training. " + shot_series)
+			print("start 2-bar training: " + shot_series)
+		engine.runAndWait()
+		loopfor(check_pause, 3, 1)
+		while 1:
+			check_pause()
+			clock = random.randrange(0, 1 + interval_range.get())
+			shotcall = random.choices(calls, weights)[0]
+			output_sentence("set up the ball", engine, -10)
+			check_pause()
+			output_sentence("time in", engine, 0)
+			loopfor(check_pause, clock, 1)
+			beep = threading.Thread(target=delayed_beep, args=((len(shotcall), beepdelay)))
+			beep.start()
+			output_sentence(shotcall, engine, 30)
+			print("%s %*d seconds" %(shotcall, 13 - len(shotcall), clock))
+			loopfor(check_pause, 2.5, 1)
+	except KillTraining:
+		print("stopped")
 
 class MyTimer():
 
@@ -128,80 +129,80 @@ class MyTimer():
 print_banner()
 
 # initialising engine and variables
-set_pause = 0
-kill_thread = threading.Thread(target=kill_handler)
-kill_thread.daemon = True
-kill_thread.start()
+# pauseswitch = 0
+# kill_thread = threading.Thread(target=kill_handler)
+# kill_thread.daemon = True
+# kill_thread.start()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-20)
 engine.setProperty('voice', voices[1].id)
 mytimer = MyTimer()
-keyboard.on_press_key("space", pause)
+# keyboard.on_press_key("space", gui.handle_pauseplay)
 
 # input reading. configuring settings
-training = input("select your training:\n\
-1: 3-bar\n2: 5-bar\n3: 2-bar\n")
-while not training == "1" and not training == "2" and not training == "3":
-	training = input("wrong input. choose 1 or 2:\n1: 3-bar training\n2: 5-bar training\n3: 2-bar training\n")
-if training == "1":
-	settings = input("do you want to use default settings or custom settings:\n\
-1: default settings\n2: custom settings\n")
-	while not settings == "1" and not settings == "2":
-		settings = input("wrong input. choose 1 or 2:\n1: default settings\n2: custom settings\n")
-	if settings == "2":
-		interval_range = input("choose the range of random time interval in seconds:\n")
-		while not interval_range.isnumeric or int(interval_range) < 0:
-			interval_range = input("wrong input. choose the range of random time interval in seconds:\n")
-		holes = input("choose how many holes (3, 5 or 7):\n")
-		while not holes == "3" and not holes == "5" and not holes == "7":
-			holes = input ("wrong input. choose how many shot options (3, 5 or 7):\n")
-		weights = list()
-		print("choose the probability of each hole (starting from your side):")
-		for hole in range(int(holes)):
-			weights.append(input("probability for hole " + str(hole + 1) + ":\n"))
-			while not weights[-1].isnumeric:
-				weights[-1] = input("wrong input. please choose a numer:\nprobability for hole" + hole + ":\n")
-			weights[-1] = float(weights[-1])
-		interval_range = int(interval_range)
-	else:
-		interval_range = 12
-		holes = "3"
-		weights = [0.33, 0.33, 0.33]
-	if holes == "3":
-		calls = ["short", "middle", "long"]
-	elif holes == "5":
-		calls = ["one", "two", "three", "four", "five"]
-	elif holes == "7":
-		calls = ["one", "two", "three", "four", "five", "six", "seven"]
-elif training == "2":
-	calls = ["lane", "wall", "overlane"]
-	weights = [0.4, 0.4, 0.1]
-	interval_range = 7
-if training == "3":
-	shot_series = input("choose a 2-bar training:\n1: pullshot\n2: pinshot\n3: bankshot\n")
-	while shot_series != "1" and shot_series != "2" and shot_series != "3":
-		shot_series = input("wrong input. choose a 2-bar training:\n1: pullshot\n2: pinshot\n3: bankshot\n")
-	if shot_series == "1":
-		shot_series = "pull shot"
-		calls = ["slice", "short square", "mid square", "spray mid", "long"]
-	if shot_series == "2":
-		shot_series = "pin shot"
-		calls = ["short", "middle", "long", "near bank", "far bank"]
-	if shot_series == "3":
-		shot_series = "bank shot"
-		calls = ["far bank", "near bank", "pullshot", "up slice", "down slice"]
-	weights = [0.2, 0.2, 0.2, 0.2, 0.2]
-	interval_range = 7
-else:
-	shot_series = None
+# training = input("select your training:\n\
+# 1: 3-bar\n2: 5-bar\n3: 2-bar\n")
+# while not training == "1" and not training == "2" and not training == "3":
+# 	training = input("wrong input. choose 1 or 2:\n1: 3-bar training\n2: 5-bar training\n3: 2-bar training\n")
+# if training == "1":
+# 	settings = input("do you want to use default settings or custom settings:\n\
+# 1: default settings\n2: custom settings\n")
+# 	while not settings == "1" and not settings == "2":
+# 		settings = input("wrong input. choose 1 or 2:\n1: default settings\n2: custom settings\n")
+# 	if settings == "2":
+# 		interval_range = input("choose the range of random time interval in seconds:\n")
+# 		while not interval_range.isnumeric or int(interval_range) < 0:
+# 			interval_range = input("wrong input. choose the range of random time interval in seconds:\n")
+# 		holes = input("choose how many holes (3, 5 or 7):\n")
+# 		while not holes == "3" and not holes == "5" and not holes == "7":
+# 			holes = input ("wrong input. choose how many shot options (3, 5 or 7):\n")
+# 		weights = list()
+# 		print("choose the probability of each hole (starting from your side):")
+# 		for hole in range(int(holes)):
+# 			weights.append(input("probability for hole " + str(hole + 1) + ":\n"))
+# 			while not weights[-1].isnumeric:
+# 				weights[-1] = input("wrong input. please choose a numer:\nprobability for hole" + hole + ":\n")
+# 			weights[-1] = float(weights[-1])
+# 		interval_range = int(interval_range)
+# 	else:
+# 		interval_range = 12
+# 		holes = "3"
+# 		weights = [0.33, 0.33, 0.33]
+# 	if holes == "3":
+# 		calls = ["short", "middle", "long"]
+# 	elif holes == "5":
+# 		calls = ["one", "two", "three", "four", "five"]
+# 	elif holes == "7":
+# 		calls = ["one", "two", "three", "four", "five", "six", "seven"]
+# elif training == "2":
+# 	calls = ["lane", "wall", "overlane"]
+# 	weights = [0.4, 0.4, 0.1]
+# 	interval_range = 7
+# if training == "3":
+# 	shot_series = input("choose a 2-bar training:\n1: pullshot\n2: pinshot\n3: bankshot\n")
+# 	while shot_series != "1" and shot_series != "2" and shot_series != "3":
+# 		shot_series = input("wrong input. choose a 2-bar training:\n1: pullshot\n2: pinshot\n3: bankshot\n")
+# 	if shot_series == "1":
+# 		shot_series = "pull shot"
+# 		calls = ["slice", "short square", "mid square", "spray mid", "long"]
+# 	if shot_series == "2":
+# 		shot_series = "pin shot"
+# 		calls = ["short", "middle", "long", "near bank", "far bank"]
+# 	if shot_series == "3":
+# 		shot_series = "bank shot"
+# 		calls = ["far bank", "near bank", "pullshot", "up slice", "down slice"]
+# 	weights = [0.2, 0.2, 0.2, 0.2, 0.2]
+# 	interval_range = 7
+# else:
+# 	shot_series = None
 
-beepdelay = input("choose the time between shotcall and beep (reaction speed):\nBeginner: 1.2\nAdvanced: 1\nPro: 0.8\n")
-while not beepdelay.isnumeric or float(beepdelay) < 0:
-	beepdelay = input("wrong input. choose the time between shotcall and beep in seconds:\n")
-beepdelay = float(beepdelay)
+# beepdelay = input("choose the time between shotcall and beep (reaction speed):\nBeginner: 1.2\nAdvanced: 1\nPro: 0.8\n")
+# while not beepdelay.isnumeric or float(beepdelay) < 0:
+# 	beepdelay = input("wrong input. choose the time between shotcall and beep in seconds:\n")
+# beepdelay = float(beepdelay)
 
-training_loop(training, engine, interval_range, weights, calls, shot_series)
+# training_loop(training, engine, interval_range, weights, calls, shot_series)
 
 
